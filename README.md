1b68056ff5c12bdd3ec887498c005655395d4ffc
Evgueni Kounik <kounik.ch@gmail.com>
2024-11-02 13:51:15 +0100

    Finished simple http server with primitive routings

---
+++ README.md
@@ -0,0 +1,53 @@
+# HTTP Server
+
+`ZTM` Udemy Course - [Complete NodeJS Developer](https://www.udemy.com/course/complete-nodejs-developer-zero-to-mastery).
+
+## Prerequisite
+
+- `Node.js` is installed ⚡
+- Prefered Code-Editor/IDE is installed (For example: `vscode`) ✍
+- Terminal is ready 😎
+
+## Description
+
+TODO
+
+## Process
+
+Install npm packages ⬇
+
+```shell
+npm install
+```
+
+Run node application 🏎️💨
+
+```shell
+npm start
+# or start with nodemon
+npm run dev
+```
+
+Clean packages 🧹🪣
+
+```shell
+npm run clean
+```
+
+Check vulnerabilities 🤔
+
+```shell
+npm audit
+```
+
+Fix vulnerabilities if needed ✅
+
+```shell
+npm audit fix
+```
+
+## Useful references
+
+- [Mozilla - HTTP request methods](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods)
+- [What is an API](https://www.mulesoft.com/api/what-is-an-api)
+- [Mozilla - HTTP response status codes](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
---
+++ index.js
@@ -0,0 +1,84 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer();
+
+// Data
+const friends = [
+  {
+    id: 0,
+    name: "Sir Isaac Newton",
+  },
+  {
+    id: 1,
+    name: "Albert Einstein",
+  },
+  {
+    id: 2,
+    name: "Marie Curie",
+  },
+  {
+    id: 3,
+    name: "Nikola Tesla",
+  },
+  {
+    id: 4,
+    name: "Charles Darwin",
+  },
+  {
+    id: 5,
+    name: "Galileo Galilei",
+  },
+  {
+    id: 6,
+    name: "Stephen Hawking",
+  },
+  {
+    id: 7,
+    name: "Ada Lovelace",
+  },
+  {
+    id: 8,
+    name: "Charles Babbage",
+  },
+  {
+    id: 9,
+    name: "Alan Turing",
+  },
+];
+
+server.on('request', (req, res) => {
+  const items = req.url.split('/');
+  // /friends/2 => ['', 'friends', '2']
+  if (items[1] === "friends") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    if (items.length === 3) {
+      // const friendIndex = +items[2];
+      const friendIndex = Number.parseInt(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (items[1] === "messages") {
+    res.setHeader("Content-Type", "text/html");
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>Hello Isaac!</li>');
+    res.write('<li>What are your thoughts on astronomy?</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+// 127.0.0.1 => localhost
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
